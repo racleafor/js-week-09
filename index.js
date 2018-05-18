b721257872b61570d5ede3d5740f85bd00aa6eb1
Rachel Forrest <rachel.forrest@lifeway.com>
2018-05-18 08:50:11 -0500

    tests passed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 function mySimpleArray() {
   // Simply return an array literal from this function
   // with any number of items
-  return 0
+  return [0, 1, 2, 3]
 }
 
 // There are two ways we've learned to access
@@ -20,43 +20,45 @@ function mySimpleArray() {
 //   2. destructuring
 // Write a function that takes an array and
 // returns the first item using one of those ways
-function head1() {
-  return 0
+function head1(daysoftheweek) {
+  return daysoftheweek[0]
 }
 
 // Write a function that does the same as the function
 // above, (that is, it takes an array and returns the
 // first item), but use the other approach
-function head2() {
-  return 0
+function head2(name) {
+  const [first, last] = name
+  return first
 }
 
 // Write a function that takes an array and returns
 // an array containing all but the first item
-function tail1() {
-  return 0
+function tail1(weekdays) {
+  const [Monday, ...Friday] = weekdays
+  return Friday
 }
 
 // Write a function similar to head1 & head2 *but*
 // using the *arguments passed into the function*
 // (simply return the first argument)
-function head3() {
-  return 0
+function head3(...name) {
+  return name[0]
 }
 
 // Write a function that takes any number of arguments
 // and returns the number of arguments passed in
 // (hints: argument gathering, p.89 and .length, p.77)
-function countMyArgs() {
-  return 0
+function countMyArgs(...name) {
+  return name.length
 }
 
 // Write a function similar to tail1 *but*
 // using the *arguments passed into the function*
 // (return all but the first argument as an array)
 // see JavaScript Allongé, p.89 for help
-function tail2() {
-  return 0
+function tail2(weekdays, ...Monday) {
+  return Monday
 }
 
 // Write a function that takes an array and does the following:
@@ -65,8 +67,9 @@ function tail2() {
 // 3. otherwise, return the length of the array
 // 4. the function should never return 0
 // Feel free to use if/else [p.31] or a ternary [p.77]
-function howMany() {
-  return 0
+function howMany(words) {
+  if (words.length === 0) return 'Empty'
+  else return words.length
 }
 
 module.exports = {
